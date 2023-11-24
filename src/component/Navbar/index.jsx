1d0f3c528eceede07586ccd476aeac44173d6022
Richie-Z <zakariarichie@yahoo.com>
2023-11-24 20:12:06 +0700

    feat(navbar): add logout

--- src/component/Navbar/index.jsx
+++ src/component/Navbar/index.jsx
@@ -2,17 +2,26 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import './style.css'
 
 function NavBar() {
+  const accessToken = localStorage.getItem('accessToken')
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+
+  const handleLoginLogout = () => {
+    if (accessToken) localStorage.removeItem('accessToken')
+    navigate('/login')
+  }
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary navbar">
       <Container fluid>
         <Link to={'/'}>
-            <Navbar.Brand>Navbar</Navbar.Brand>
+          <Navbar.Brand>Navbar</Navbar.Brand>
         </Link>
-        
+
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -21,13 +30,13 @@ function NavBar() {
             navbarScroll
           >
             <Link to={'/'}>
-                <Nav>Home</Nav>
+              <Nav>Home</Nav>
             </Link>
           </Nav>
 
-          <Link to={'/login'}>
-            <Button variant="outline-success">LOGIN</Button>
-          </Link>
+          {pathname !== '/login' && (
+            <Button variant={accessToken ? "outline-danger" : "outline-success"} onClick={handleLoginLogout}>{accessToken ? 'Logout' : 'Login'}</Button>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
